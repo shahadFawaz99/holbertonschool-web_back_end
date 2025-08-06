b21ed7725521190c54928d3e4c48c567b2214b6c
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-06 04:12:21 +0300

    Update 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""This module provides a function to add two float numbers with type annotations."""
+
 def add(a: float, b: float) -> float:
-    """Returns the sum of two float numbers"""
+    """Returns the sum of two float numbers."""
     return a + b
