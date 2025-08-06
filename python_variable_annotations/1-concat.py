b548c04c17464a412198bb1caf4f535a829eb45c
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-06 04:18:54 +0300

    Update 1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module provides a function to concatenate two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenation of two strings."""
+    return str1 + str2
