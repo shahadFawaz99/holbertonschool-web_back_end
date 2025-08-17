9ad22c4a10b026db812ef0884f915b0955479dbd
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-17 11:25:27 +0300

    Update 1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,1 +1,54 @@
+#!/usr/bin/env python3
+import csv
+from typing import List
+
+
+# helper function from task 0
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Return start and end index for a given page and page_size
+    """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Load and cache dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            # remove header row
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Return a specific page of the dataset
+        - page and page_size must be positive integers
+        - use index_range to get start and end
+        - return [] if start index is out of range
+        """
+        # check input validity
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+
+        if start >= len(data):
+            return []
+
+        return data[start:end]
 
