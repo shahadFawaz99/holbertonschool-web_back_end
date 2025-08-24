76b27fc222557e43cc17430bb9087ce7bb5873bf
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-24 20:04:49 +0300

    Update 6-sky_high.js

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);        // تمرير sqft للـ parent class
+    this._floors = floors;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Override evacuationWarningMessage
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
