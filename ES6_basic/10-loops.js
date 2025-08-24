606dbcfe0fbe1b406955936e41c724ac1fc708e3
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-24 18:43:27 +0300

    Update 10-loops.js

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let i = 0; i < array.length; i++) {
-    array[i] = appendString + array[i];
+    const newArray = [];
+    for (const value of array) {
+      newArray.push(appendString + value);
+    }
+    return newArray;
   }
-  return array;
-}
