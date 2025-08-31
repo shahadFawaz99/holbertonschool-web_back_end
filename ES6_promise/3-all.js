260c6e45cf2392d3fc1e4df6dc8faeb87b466bb7
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-31 21:27:13 +0300

    Update 3-all.js

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,1 +1,11 @@
-
+// This function resolves multiple promises for user signup
+import { uploadPhoto, createUser } from './utils.js';
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
