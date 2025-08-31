cd5f8e888a654f865a97843125e4e9c519aa7e85
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-31 21:34:04 +0300

    Update 7-load_balancer.js

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,1 +1,4 @@
-
+// This function returns the value of the promise that resolves first
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload]);
+}
