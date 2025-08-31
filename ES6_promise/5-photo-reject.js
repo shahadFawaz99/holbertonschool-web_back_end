f32acb16e6f4d35a5e6a8ab382ec16fb91173ee9
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-31 21:29:09 +0300

    Update 5-photo-reject.js

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,1 +1,4 @@
-
+// This function returns a rejected Promise with an Error containing the fileName
+export default function uploadPhoto(fileName) {
+  return Promise.reject(new Error(`${fileName} cannot be processed`));
+}
