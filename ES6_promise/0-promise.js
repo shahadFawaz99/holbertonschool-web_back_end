50dd310264f89b9250ff8c5de2df2b2831ef1a98
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-31 21:23:02 +0300

    Update 0-promise.js

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,1 +1,7 @@
-
+// This function returns a Promise
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // You can resolve immediately or after some async operation
+    resolve('Success');
+  });
+}
