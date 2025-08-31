684cda078103f93d664b528669e0ab7084125116
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-31 21:33:08 +0300

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,16 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-// This function handles multiple promises and returns their settled status
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
   return Promise.allSettled(promises).then((results) =>
-    results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return { status: 'fulfilled', value: result.value };
+    results.map((res) => {
+      if (res.status === 'fulfilled') {
+        return { status: res.status, value: res.value };
+      } else {
+        return { status: res.status, value: res.reason.toString() };
       }
-      return { status: 'rejected', value: result.reason };
     })
   );
 }
