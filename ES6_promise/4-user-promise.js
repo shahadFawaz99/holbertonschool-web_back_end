93927a146dce4540c81c08fdfe86f23b4b09edfd
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-31 21:28:09 +0300

    Update 4-user-promise.js

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,1 +1,4 @@
-
+// This function returns a resolved promise with an object containing firstName and lastName
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName, lastName });
+}
