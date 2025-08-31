3103f4578dea18925c1c2b19bbc110e426a626d4
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-31 20:57:23 +0300

    Update 9-groceries_list.js

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+// This function returns a Map of groceries with their quantities
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
