dad6cac4ae230afb9b53d1beb44cb93bce7454f8
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-31 20:53:26 +0300

    Update 6-set.js

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,6 @@
+// This function creates a Set from an array
+// Parameter: array (any type of elements)
+// Returns: a Set containing the unique values of the array
+export default function setFromArray(array) {
+  return new Set(array);
+}
