3725b6c09c22f293f1d77675a68b7665b3af7d83
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-31 21:24:38 +0300

    Update 1-promise.js

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,1 +1,12 @@
-
+// This function returns a Promise based on the success parameter
+// If success is true: resolves with { status: 200, body: 'Success' }
+// If success is false: rejects with an Error
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
