0d738ab6d3e8135c5e094b460f6556707b5a084a
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-31 21:35:07 +0300

    Update 8-try.js

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,1 +1,8 @@
-
+// This function divides numerator by denominator
+// Throws an error if denominator is 0
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
