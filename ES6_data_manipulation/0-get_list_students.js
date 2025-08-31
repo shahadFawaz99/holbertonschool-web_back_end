170ccc960ae760e01c839df1916bcac645339416
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-08-31 20:46:48 +0300

    Update 0-get_list_students.js

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+// This function returns an array of student objects
+// Each student object contains: id (Number), firstName (String), and location (String)
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
